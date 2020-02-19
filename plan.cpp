df96281e1d4ab868da69d9c6584beb3d6cb6345b
qwertyhoge <cood.gh47@hotmail.co.jp>
2020-02-19 15:27:06 +0900

    made a simply styled test plan without proper alignment

--- plan.cpp
+++ plan.cpp
@@ -12,16 +12,26 @@ Plan::Plan(QWidget *parent) : QWidget(parent), planName(""), startTime(0), endTi
 Plan::Plan(QWidget *parent, QString name, PlanTime start, PlanTime end)
   : QWidget(parent), planName(name), startTime(start), endTime(end)
 {
-  setStyleSheet("background-color: #f7eff2;");
+  setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+
+  QPalette plt = palette();
+  plt.setColor(QPalette::Background, QColor(225, 255, 252));
+  setAutoFillBackground(true);
+  setPalette(plt);
 
   QVBoxLayout *layout = new QVBoxLayout();
-  layout->setSpacing(6);
+  layout->setSpacing(4);
+  layout->setContentsMargins(0, 6, 0, 6);
   setLayout(layout);
 
   QLabel *nameLabel = new QLabel(planName);
+  nameLabel->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+  nameLabel->setAlignment(Qt::AlignCenter);
+  QFont font;
+  font.setPointSize(12);
+  nameLabel->setFont(font);
   layout->addWidget(nameLabel);
 
-  /*
   QFrame *horLine = new QFrame();
   horLine->setFrameShape(QFrame::HLine);
   horLine->setFrameShadow(QFrame::Sunken);
@@ -29,12 +39,11 @@ Plan::Plan(QWidget *parent, QString name, PlanTime start, PlanTime end)
   horLine->setFixedHeight(2);
   horLine->setLineWidth(2);
   layout->addWidget(horLine);
-*/
-  QLabel *timeLabel = new QLabel(start.toString() + " - " + end.toString());
-  layout->addWidget(timeLabel);
 
   layout->addStretch(1);
 
-  QLabel *extraLabel = new QLabel("test");
-  layout->addWidget(extraLabel);
+  QLabel *timeLabel = new QLabel(start.toString() + " - " + end.toString());
+  layout->addWidget(timeLabel);
+  timeLabel->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+  timeLabel->setAlignment(Qt::AlignRight | Qt::AlignBottom);
 }
